1f270ec515812af328ede10b3c19bcc5ccdd49c4
Jhonatan <jhonatanchoqueticllaflores@gmail.com>
2024-10-20 15:37:44 -0400

    navBar

--- app/page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div className='flex justify-center '>My page</div>
-  )
-}
- 
---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex justify-center '>
+      <nav className='w-full h-20 bg-blue-600 flex justify-between items-center px-10'>
+        <h2>DEMO Streaming</h2>
+        <div>
+          <button className='text-white text-md'>Log in</button>
+          <button className='bg-black text-white p-3'>Start your free trial</button>
+        </div>
+      </nav>
+    </div>
+
+  )
+}
+ 
